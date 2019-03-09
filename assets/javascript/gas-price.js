1aee873123236a5b617a77ba32802fb27461777d
BrendanG <gutleberb@gmail.com>
2019-03-09 12:51:39 -0600

    added ajax call to get current gas price

--- assets/javascript/gas-price.js
+++ assets/javascript/gas-price.js
@@ -4,7 +4,7 @@
 
 var queryOrigin = "nashville tennessee";
 var queryDestination = "Middletown new jersey";
-var gasPrice = 0;
+var gasPrice = 3;
 var milesPerGallon = 0;
 var gallonsPerTank = 0;
 
@@ -23,13 +23,33 @@ $.ajax({
     var distance = results[0].elements[0].distance.text
 
     console.log(distance)
+
+    var totalGasCost = 0;
+    var gallonsNeeded = distance / milesPerGallon;
+
+    totalGasCost = gallonsNeeded * gasPrice;
 }, function (errorObject) {
     console.log("the read failed:" + errorObject.code)
 });
 
-var totalGasCost = 0;
-var gallonsNeeded = distance / milesPerGallon;
 
-totalGasCost = gallonsNeeded * gasPrice;
+
 
 //}); //close document.ready
+
+
+
+$.ajax({
+
+    url: proxyURL + "https://www.fueleconomy.gov/ws/rest/fuelprices/",
+    method: "GET",
+    dataType: "JSON"
+
+}).then(function (response) {
+
+    fuelPrice = response.regular
+
+    console.log(fuelPrice)
+}, function (errorObject) {
+    console.log("the read failed:" + errorObject.code)
+});
